5e5a9cc096e17fdaaef00d2636832c4f96ad8764
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2024-09-30 16:51:51 +0530

    Updated Java version to 11 and saved db data to volumes.

--- order-service/src/main/java/com/example/order/model/Order.java
+++ order-service/src/main/java/com/example/order/model/Order.java
@@ -1,10 +1,10 @@
 package com.example.order.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name = "orders")
