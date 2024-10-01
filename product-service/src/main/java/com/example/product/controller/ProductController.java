d424c051e17d09223c921da6c9791586d970bd27
Sapan Bhandari <sapanbhandari01@gmail.com>
2024-10-01 17:31:27 +0530

    Added Slf4j loggers using lombok in product-service and swagger in both

--- product-service/src/main/java/com/example/product/controller/ProductController.java
+++ product-service/src/main/java/com/example/product/controller/ProductController.java
@@ -4,22 +4,26 @@ import com.example.product.model.Product;
 import com.example.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@Slf4j
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
     @GetMapping
     public List<Product> getAllProducts() {
+    	log.info("Fetching all products");
         return productRepository.findAll();
     }
 
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
+    	log.info("Creating a new product");
         return productRepository.save(product);
     }
 }
--- product-service/src/main/java/com/example/product/model/Product.java
+++ product-service/src/main/java/com/example/product/model/Product.java
@@ -4,31 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private Double price;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Double getPrice() {
-		return price;
-	}
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
 }
